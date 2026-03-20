2bc05975df7281b77e56fac216bebb1ee13366ad
LRDEnE <lordenryque.dev@gmail.com>
2026-03-21 00:09:55 +0100

    chore: add open-next.config.ts to satisfy opennext requirement

---
+++ open-next.config.ts
@@ -0,0 +1,22 @@
+const config = {
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+    },
+  },
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+    },
+  },
+  dangerous: {
+    enableCacheInterception: false,
+  },
+};
+
+export default config;
